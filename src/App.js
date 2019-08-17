3776bf3d2c241c6f0b573753cfd6e6495a1f4cc4
Vinicius Rocha <viniciusrocha@Vini-MacBook.local>
2019-08-16 23:22:15 -0400

    I think I did it

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Pipeline is working</p>
+        <p>Pipeline is (actually) working</p>
         <a
           className="App-link"
           href="https://reactjs.org"
